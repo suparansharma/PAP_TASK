e5c1e6af0cae8bb9c1bd9730e9fdde5c28bfeb61
suparansharma <suparansharmap@gmail.com>
2022-06-09 17:04:51 +0600

    directional sytem updated

--- map_react_api_system/src/App.js
+++ map_react_api_system/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Map from './components/Map/Map';
+import Direction from './components/Map/Direction';
 
 function App() {
   return (
     <div className="App">
-     <Map></Map>
+     {/* <Map></Map> */}
+     <Direction></Direction>
     </div>
   );
 }
